68e154a736a0106e7ce6dc1bd3439d6b8e56c1ab
Surearty <131195393+Surearty@users.noreply.github.com>
2024-03-30 02:31:40 +0300

    bag fix

--- day_7/Hangman.py
+++ day_7/Hangman.py
@@ -1,9 +1,11 @@
 import random
-import hangman_art
-import hangman_words
-print(hangman_art.logo)
+from hangman_art import logo
+from hangman_art import stages
+from hangman_words import word_list
+
+print(logo)
 lives = 6
-chosen_word = random.choice(hangman_words.word_list).lower()
+chosen_word = random.choice(word_list).lower()
 print(f'Pssst, the solution is {chosen_word}.')
 your_ans = ['_'] * len(chosen_word)
 game = True
@@ -15,7 +17,7 @@ while (''.join(your_ans) != chosen_word and game):
     if guess not in chosen_word:
         lives -= 1
         print(your_ans)
-        print(hangman_art.stages[lives])
+        print(stages[lives])
         if lives == 0:
             game = False
     else:
