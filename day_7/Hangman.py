bfa33e124d5d8bfe3b3ad9d2fc60490558f832e3
Surearty <131195393+Surearty@users.noreply.github.com>
2024-03-30 02:03:20 +0300

    step 4

--- day_7/Hangman.py
+++ day_7/Hangman.py
@@ -1,22 +1,83 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
-
-# Randomly choose a word from the word_list and assign it to a
-#  variable called chosen_word.
-
-# Ask the user to guess a letter and assign their answer to a
-# variable called guess. Make guess lowercase.
-
-# Check if the letter the user guessed (guess) is one of the
-# letters in the chosen_word.
-
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+lives = 6
 chosen_word = random.choice(word_list).lower()
 print(f'Pssst, the solution is {chosen_word}.')
 your_ans = ['_'] * len(chosen_word)
-while (''.join(your_ans) != chosen_word):
+game = True
+while (''.join(your_ans) != chosen_word and lives > 0):
     guess = input('guess a letter:').lower()
+    guess_num = 0
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
             your_ans[i] = guess
-            print(your_ans)
-
+            guess_num += 1
+    if guess_num == 0:
+        lives -= 1
+        print(stages[lives])
+        print(your_ans)
+        if lives == 0:
+            game = False
+            break
+    else:
+        print(your_ans)
+if game:
+    print('YOU WIN!')
+else:
+    print('YOU LOOOSE!')
