6b096edd149c58c2c8d3228d88fdb0fe7bb40b84
Surearty <131195393+Surearty@users.noreply.github.com>
2024-03-30 02:14:55 +0300

    bag fix

--- day_7/Hangman.py
+++ day_7/Hangman.py
@@ -61,20 +61,17 @@ chosen_word = random.choice(word_list).lower()
 print(f'Pssst, the solution is {chosen_word}.')
 your_ans = ['_'] * len(chosen_word)
 game = True
-while (''.join(your_ans) != chosen_word and lives > 0):
+while (''.join(your_ans) != chosen_word and game):
     guess = input('guess a letter:').lower()
-    guess_num = 0
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
             your_ans[i] = guess
-            guess_num += 1
-    if guess_num == 0:
+    if guess not in chosen_word:
         lives -= 1
-        print(stages[lives])
         print(your_ans)
+        print(stages[lives])
         if lives == 0:
             game = False
-            break
     else:
         print(your_ans)
 if game:
