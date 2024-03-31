9b3c57466006be3d92d8d249d71b6c8d02561a66
Surearty <131195393+Surearty@users.noreply.github.com>
2024-03-31 19:49:39 +0300

    step 3

--- day_8/caesar_cipher/main.py
+++ day_8/caesar_cipher/main.py
@@ -1,21 +1,13 @@
-def encrypt(message, shift):
+def caesar(message, shift, direction):
     new_message = ""
+    if direction == 2:
+        shift = -shift
     for chr in message:
         if chr.isalpha():
             ch = alphabet.index(chr) + shift
             if ch > 25:
                 ch = ch-26
-            new_message += alphabet[ch]
-        else:
-            new_message += chr
-    print(new_message)
-
-def decrypt(message, shirt):
-    new_message = ""
-    for chr in message:
-        if chr.isalpha():
-            ch = alphabet.index(chr) - shift
-            if ch < 0:
+            elif ch < 0:
                 ch = 26 + ch
             new_message += alphabet[ch]
         else:
@@ -58,9 +50,5 @@ direction = int(input("Type '1' to encrypt, type '2' to decrypt:\n"))
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == 1:
-    encrypt(text, shift)
-elif direction == 2:
-    decrypt(text, shift)
-else:
-    print('Unknown command!')
+caesar(text, shift, direction)
+caesar(text, shift, direction)
