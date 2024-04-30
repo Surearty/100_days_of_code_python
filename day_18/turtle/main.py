cecfb5edae4a8f8960d05a4dcbde12640e7c57cb
Surearty <131195393+Surearty@users.noreply.github.com>
2024-04-30 20:33:39 +0300

    paint

--- day_18/turtle/main.py
+++ day_18/turtle/main.py
@@ -1,28 +1,26 @@
+
+import turtle
 from turtle import Turtle, Screen
+turtle.colormode(255)
 
+color_list = [(249, 228, 17), (213, 13, 9), (198, 12, 35), (231, 228, 5), (197, 69, 20), (33, 90, 188), (43, 212, 71), (234, 148, 40), (33, 30, 152), (16, 22, 55), (66, 9, 49), (240, 245, 251), (244, 39, 149), (65, 202, 229), (14, 205, 222), (63, 21, 10), (224, 19, 111), (229, 165, 8), (15, 154, 22), (245, 58, 16), (98, 75, 9), (248, 11, 9), (222, 140, 203), (68, 240, 161), (10, 97, 62), (5, 38, 33), (68, 219, 155), (238, 157, 212), (86, 77, 208), (86, 225, 235), (250, 8, 14), (242, 166, 157), (177, 180, 224), (36, 243, 159), (6, 81, 115), (11, 55, 248)]
 tim = Turtle()
-tim.shape('triangle')
-tim.color('#b144bf')
-
-# for _ in range (4):
-#     tim.forward(100)
-#     tim.right(90)
-
-#CHALLANGE 3
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
-           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-for i in range(3, 10):
-    tim.pencolor(colors[i-3])
-    dg = 360 / i
-    for _ in range (i):
+tim.speed(2)
+color_num = 0
+y = -200
+tim.penup()
+tim.hideturtle()
+for i in range(10):
+    tim.setpos(-200, i*50 + y)
+    for _ in range (10):
+        tim.dot(20, color_list[color_num])
+        color_num+=1
+        if color_num >= len(color_list):
+            color_num = 0
         tim.forward(50)
-        tim.right(dg)
-    i += 1
-
-
-
-
-
 
 screen = Screen()
 screen.exitonclick()
+
+
+
