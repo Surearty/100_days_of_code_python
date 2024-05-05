fade686b6e43f3f35728e64e50da6b7d226bf5d0
Surearty <131195393+Surearty@users.noreply.github.com>
2024-05-05 13:49:06 +0300

    finish the game

--- TurtleCrossing game/main.py
+++ TurtleCrossing game/main.py
@@ -8,7 +8,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle Crossing Game")
 screen.tracer(0)
-
+scoreboard = Scoreboard()
 player = Player()
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -23,6 +23,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 #detect collision with succes
     if player.got_finish():
         player.go_to_start()
